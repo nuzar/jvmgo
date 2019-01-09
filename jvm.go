ab3af8e38d46b1d90b86f61bcf4fc92da3b6c239
Nuzar <liuyunfeng312@163.com>
2019-01-09 21:10:57 +0800

    classpath

--- jvm.go
+++ jvm.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"jvmgo/classpath"
 	"jvmgo/cmdline"
+	"strings"
 )
 
 func startJVM(cmd *cmdline.Cmd) {
-	fmt.Println("start " + cmd.Class)
+	cp := classpath.Parse(cmd.XjreOption, cmd.CpOption)
+	fmt.Printf("classpath: %v class: %v args:%v\n",
+		cp, cmd.Class, cmd.Args)
+
+	classname := strings.Replace(cmd.Class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(classname)
+	if err != nil {
+		fmt.Printf("Could not find or load main class %s\n", cmd.Class)
+		return
+	}
+
+	fmt.Printf("class data: %v\n", classData)
 }
